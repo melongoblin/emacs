546c752c4425023d25f2560c0b76b8739cc8a30e
Melon Goblin <119814693+melongoblin@users.noreply.github.com>
2023-01-09 10:40:00 -0500

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -8,7 +8,7 @@
 (unless (and (display-graphic-p) (eq system-type 'darwin))
   (push '(menu-bar-lines . 0) default-frame-alist))
 (push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-vars) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 
 (setq-default frame-title-format '("%b - Emacs"))
 
